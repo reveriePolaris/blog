1723fceb1b4260c581d2148747e3d4b7241fe9fe
reveriePolaris <sean.cardenas2@gmail.com>
2025-11-23 18:04:54 -0500

    updated

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,13 @@
+import { HtmlBasePlugin } from "@11ty/eleventy"
+
 export default async function(eleventyConfig) {
     eleventyConfig.setInputDirectory("src");
-    eleventyConfig.setOutputDirectory("output");
+    eleventyConfig.setOutputDirectory("docs");
     eleventyConfig.setIncludesDirectory("include");
     eleventyConfig.addPassthroughCopy("src/css");
+    eleventyConfig.addPlugin(HtmlBasePlugin)
+}
+
+export const config = {
+    pathPrefix: "/blog/"
 }
